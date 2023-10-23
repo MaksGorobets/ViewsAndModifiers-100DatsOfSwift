4d7cea2565266125c589e274473108e69fd04b68
MaksGorobets <gorobets542@gmail.com>
2023-10-24 00:55:42 +0300

    Project 3 part 2 update

---
+++ ViewsAndModifiers/TitleModifierView.swift
@@ -0,0 +1,18 @@
+//
+//  TitleModifierView.swift
+//  ViewsAndModifiers
+//
+//  Created by Maks Winters on 24.10.2023.
+//
+
+import SwiftUI
+
+struct TitleModifierView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TitleModifierView()
+}
